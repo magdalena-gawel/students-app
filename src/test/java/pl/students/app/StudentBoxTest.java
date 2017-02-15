0d9f5cf0de59fbc7f646dc49a9ea23f2aa2bce80
Paweł Słowik <pawel.slowik@schibsted.pl>
2017-02-15 12:56:59 +0100

    StudentsBox + StudentBox

--- src/test/java/pl/students/app/StudentBoxTest.java
+++ src/test/java/pl/students/app/StudentBoxTest.java
@@ -8,7 +8,7 @@ public class StudentBoxTest {
 
     @Test
     public void testEmptyBox() {
-        StudentBox emptyBox = new StudentBox(null);
+        StudentBox emptyBox = new StudentBox<>(null);
         assertEquals(null, emptyBox.whatsInTheBox());
         assertEquals(false, emptyBox.isPresent());
     }
@@ -16,7 +16,7 @@ public class StudentBoxTest {
     @Test
     public void testStudentSuper() {
         Student student = new Student("Jakub", "Tokarczyk");
-        StudentBox filledBox = new StudentBox(student);
+        StudentBox filledBox = new StudentBox<>(student);
         assertEquals(student, filledBox.whatsInTheBox());
         assertEquals(true, filledBox.isPresent());
     }
@@ -24,7 +24,9 @@ public class StudentBoxTest {
     @Test
     public void testStudentChild() {
         Student studentA = new StudentGroupA("Jakub", "Tokarczyk");
-        StudentBox filledBox = new StudentBox(studentA);
+        //wont compile
+        //StudentBox filledBox = new StudentBox<>(0.0);
+        StudentBox filledBox = new StudentBox<>(studentA);
         assertEquals(studentA, filledBox.whatsInTheBox());
         assertEquals(true, filledBox.isPresent());
     }
--- src/test/java/pl/students/app/StudentListBoxTest.java
+++ src/test/java/pl/students/app/StudentListBoxTest.java
@@ -12,8 +12,11 @@ public class StudentListBoxTest {
     @Test
     public void testStudentSuper() {
         Student student = new Student("Jakub", "Tokarczyk");
-        List<Student> list = new ArrayList<>();
+        //warning
+        List<Student> list = new ArrayList();
         list.add(student);
+        //wont work
+        //StudentListBox<Student> filledBox = new StudentListBox<>(new ArrayList<Double>()); //question: why does it work? how to restrict type?
         StudentListBox<Student> filledBox = new StudentListBox<>(list); //question: why does it work? how to restrict type?
         assertEquals(list, filledBox.whatsInTheBox());
     }
